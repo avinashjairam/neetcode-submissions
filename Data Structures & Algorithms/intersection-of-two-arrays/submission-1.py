8012c6ee2d9c8e68ec1246d0ecc6af2e8a70e23b
Avinash Jairam <avinashjairam@users.noreply.github.com>
2026-07-18 16:44:11 -0400

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = set()
+
+        nums2.sort()
+
+        def binary_search(x):
+            l = 0 
+            r = len(nums2) - 1 
+
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums2[mid] == x:
+                    return True
+                elif nums2[mid] < x:
+                    l = mid + 1
+                else:
+                    r = mid - 1 
+
+            return False 
+
+        for x in nums1:
+            if binary_search(x):
+                if x not in result:
+                    result.add(x)
+
+        return list(result) 
