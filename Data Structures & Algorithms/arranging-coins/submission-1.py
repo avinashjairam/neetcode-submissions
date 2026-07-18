16d657391be285a9dc3e6a513661e338ac8d29d9
Avinash Jairam <avinashjairam@users.noreply.github.com>
2026-07-18 18:33:25 -0400

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        floors = 0 
+        coins_needed_next_floor = 1 
+
+        while n - coins_needed_next_floor >= 0 :
+            floors += 1 
+            coins_needed_next_floor += floors + 1
+
+        return floors 
